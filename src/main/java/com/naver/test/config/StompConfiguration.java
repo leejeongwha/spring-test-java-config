f6a4378d6dddd1f119a3858c4834beea48d3589d
leejeongwha <ljk2491@naver.com>
2016-01-08 16:27:28 +0900

    주기적으로 broadcasting 하는 부분 추가

--- src/main/java/com/naver/test/config/StompConfiguration.java
+++ src/main/java/com/naver/test/config/StompConfiguration.java
@@ -1,11 +1,15 @@
 package com.naver.test.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
+@Configuration
 @EnableWebSocketMessageBroker
+@EnableScheduling
 public class StompConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
 
 	@Override
--- src/main/java/com/naver/test/websock/controller/StompController.java
+++ src/main/java/com/naver/test/websock/controller/StompController.java
@@ -1,9 +1,14 @@
 package com.naver.test.websock.controller;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +17,9 @@ import com.naver.test.websock.model.Message;
 @Controller
 public class StompController {
 	private final Logger logger = LoggerFactory.getLogger(StompController.class);
+
+	@Autowired
+	public SimpMessageSendingOperations messagingTemplate;
 
 	@RequestMapping("stomp")
 	public String stomp() {
@@ -24,5 +32,19 @@ public class StompController {
 		logger.info("stomp message received!!");
 		message.setContent("stomp echo : " + message.getContent());
 		return message;
+	}
+
+	/**
+	 * 10초마다 broadcasting
+	 */
+	@Scheduled(cron = "*/10 * * * * ?")
+	public void sendMessage() {
+		String strMessage = "현재 시작은 " + new Date() + " 입니다.";
+		Message message = new Message();
+		message.setContent(strMessage);
+
+		logger.info(strMessage);
+
+		messagingTemplate.convertAndSend("/topic/greetings", message);
 	}
 }
