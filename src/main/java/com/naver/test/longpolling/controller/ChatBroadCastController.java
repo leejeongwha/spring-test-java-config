0e68fd496207b9eede1c0c1f5301a9bfd33ccf57
leejeongwha <ljk2491@naver.com>
2016-01-13 11:00:28 +0900

    롱폴링 커밋

--- src/main/java/com/naver/test/longpolling/controller/ChatBroadCastController.java
+++ src/main/java/com/naver/test/longpolling/controller/ChatBroadCastController.java
@@ -21,16 +21,34 @@ public class ChatBroadCastController {
 	private final Queue<DeferredResult<Message>> responseBodyQueue = new ConcurrentLinkedQueue<DeferredResult<Message>>();
 
 	/**
-	 * 아래와 같이 톰캣 서버의 Connector asyncTimeout 설정 늘려줄 필요 있음
+	 * 아래와 같이 톰캣 server.xml의 Connector 엘리먼트의 asyncTimeout 설정 늘려줄 필요 있음. JSP에서
+	 * 30초마다 호출하므로 40초로 설정하면 최대 2개의 커넥션 유지(default:10초)
 	 * <Connector connectionTimeout="20000" port="8080" protocol="HTTP/1.1"
-	 * redirectPort="8443" asyncTimeout="90000"/>
+	 * redirectPort="8443" asyncTimeout="40000"/>
 	 * 
 	 * @return
 	 */
 	@RequestMapping(value = "/broadcast", method = RequestMethod.GET)
 	@ResponseBody
 	public DeferredResult<Message> broadcast() {
+		logger.info("broadcast request & check queue size  : {}", responseBodyQueue.size());
 		DeferredResult<Message> result = new DeferredResult<Message>();
+
+		result.onCompletion(new Runnable() {
+			@Override
+			public void run() {
+				// timeout 이나 네트웍 에러로 인하여 더이상 사용가능하지 않을 때 컨테이너 스레드에 의해 호출
+				logger.info("complete & queue remove : {}", responseBodyQueue.remove(result));
+			}
+		});
+
+		result.onTimeout(new Runnable() {
+			@Override
+			public void run() {
+				// 비동기 요청이 타임아웃 되었을 때 컨테이너 스레드에 의해 호출
+				logger.info("timeout & queue remove : {}", responseBodyQueue.remove(result));
+			}
+		});
 
 		responseBodyQueue.add(result);
 
@@ -45,11 +63,12 @@ public class ChatBroadCastController {
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	@ResponseBody
 	public String add(Message message) {
-		logger.info(message.getContent());
+		logger.info("send message : {}", message.getContent());
 
 		for (DeferredResult<Message> result : this.responseBodyQueue) {
+			// spring mvc가 관리하는 스레드가 컨테이너 스레드에게 알림
 			result.setResult(message);
-			this.responseBodyQueue.remove(result);
+			responseBodyQueue.remove(result);
 		}
 
 		return "success";
